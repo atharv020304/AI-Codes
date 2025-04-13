5f184e87a4db9a9c7c43648ea3d281b7ece07f7d
Atharv Bapat <atharvabapat3@gmail.com>
2025-04-13 18:24:05 +0530

    dfs bfs

---
+++ BFS&DFS-Graph/Graph.cpp
@@ -0,0 +1,83 @@
+#include <iostream>
+#include <unordered_map>
+#include <vector>
+#include <queue>
+#include <stack>
+
+using namespace std;
+
+class Graph {
+public:
+    unordered_map<int, vector<int>> adj;
+
+    void addEdge(int u, int v, bool directed = false) {
+        adj[u].push_back(v);
+        if (!directed) {
+            adj[v].push_back(u);
+        }
+    }
+
+    void bfs(int start, int n) {
+        vector<bool> visited(n + 1, false);
+        queue<int> q;
+
+        q.push(start);
+        visited[start] = true;
+
+        cout << "BFS: ";
+        while (!q.empty()) {
+            int node = q.front();
+            q.pop();
+            cout << node << " ";
+
+            for (int neighbor : adj[node]) {
+                if (!visited[neighbor]) {
+                    q.push(neighbor);
+                    visited[neighbor] = true;
+                }
+            }
+        }
+        cout << endl;
+    }
+
+    void dfs(int start, int n) {
+        vector<bool> visited(n + 1, false);
+        stack<int> s;
+
+        s.push(start);
+        visited[start] = true;
+
+        cout << "DFS: ";
+        while (!s.empty()) {
+            int node = s.top();
+            s.pop();
+            cout << node << " ";
+
+            for (auto it = adj[node].rbegin(); it != adj[node].rend(); ++it) {
+                if (!visited[*it]) {
+                    s.push(*it);
+                    visited[*it] = true;
+                }
+            }
+        }
+        cout << endl;
+    }
+};
+
+int main() {
+    Graph g;
+
+    g.addEdge(1, 2);
+    g.addEdge(1, 3);
+    g.addEdge(2, 4);
+    g.addEdge(2, 5);
+    g.addEdge(3, 6);
+    g.addEdge(3, 7);
+
+    int totalNodes = 7;
+
+    g.bfs(1, totalNodes);
+    g.dfs(1, totalNodes);
+
+    return 0;
+}
---
+++ BFS&DFS-Tree/tree.cpp
@@ -0,0 +1,134 @@
+#include<iostream>
+#include<vector>
+#include<queue>
+#include<stack>
+#include<unordered_map>
+#include<algorithm>
+
+
+using namespace std;
+
+
+class node{
+    public:
+    int data;
+    node* left;
+    node* right;
+};
+
+
+
+
+void bfs(node* root){
+    if(root == NULL){
+        return;
+    }
+    queue<node*> q;
+    q.push(root);
+    while(!q.empty()){
+        node* temp = q.front();
+        q.pop();
+        cout << temp->data << " ";
+        if(temp->left != NULL){
+            q.push(temp->left);
+        }
+        if(temp->right != NULL){
+            q.push(temp->right);
+        }
+    }
+}
+
+
+void bfs2(node* root){
+    if(root == NULL){
+        return;
+    }
+    queue<node*> q;
+    q.push(root);
+    while(!q.empty()){
+        node* temp = q.front();
+        q.pop();
+        cout << temp->data << " ";
+        if(temp->right != NULL){
+            q.push(temp->right);
+        }
+        if(temp->left != NULL){
+            q.push(temp->left);
+        }
+       
+    }
+}
+
+
+void dfs(node* root){
+    if(root == NULL){
+        return;
+    }
+    stack<node*> s;
+    s.push(root);
+    while(!s.empty()){
+        node* temp = s.top();
+        s.pop();
+        cout << temp->data << " ";
+        if(temp->right != NULL){
+            s.push(temp->right);
+        }
+        if(temp->left != NULL){
+            s.push(temp->left);
+        }
+    }
+}
+
+
+void dfs2(node* root){
+    if(root == NULL){
+        return;
+    }
+    stack<node*> s;
+    s.push(root);
+    while(!s.empty()){
+        node* temp = s.top();
+        s.pop();
+        cout << temp->data << " ";
+        if(temp->left != NULL){
+            s.push(temp->left);
+        }
+        if(temp->right != NULL){
+            s.push(temp->right);
+        }
+       
+    }
+}
+
+
+
+
+
+
+
+
+int main(){
+    node* root = new node();
+    root->data = 1;
+    root->left = new node();
+    root->left->data = 2;
+    root->left->left = new node();
+    root->left->left->data = 3;
+    root->left->right = new node();
+    root->left->right->data = 4;
+    root->right = new node();
+    root->right->data = 5;
+    root->right->left = new node();
+    root->right->left->data = 6;
+    root->right->right = new node();
+    root->right->right->data = 7;
+
+
+    bfs2(root);
+    cout << endl;
+    bfs(root);
+    cout << endl;
+    dfs(root);
+    cout << endl;
+    dfs2(root);
+}
